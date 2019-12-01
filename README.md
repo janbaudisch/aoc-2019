ca4efb11ce7b71903a89fefa4f3dc7d943bd05d9
Jan Baudisch <dev@baudisch.xyz>
2019-12-01 18:05:46 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Build][build-img]][build-url]
+
+# Advent of Code 2019
+
+> My Rust implementation of [Advent of Code 2019][aoc-url].
+
+[build-img]: https://travis-ci.com/janbaudisch/aoc-2018.svg?branch=master
+[build-url]: https://img.shields.io/github/workflow/status/janbaudisch/aoc-2019/Test?label=Build
+[aoc-url]: https://adventofcode.com/2019
---
+++ day_01/src/main.rs
@@ -0,0 +1,31 @@
+use common::input;
+
+fn main() {
+    let mut fuel: Vec<u32> = input::read_lines()
+        .iter()
+        .map(|x| u32::from_str_radix(&x, 10).expect("error converting input"))
+        .map(|x| calculate_fuel(&x) as u32)
+        .collect();
+
+    println!("[PART ONE] sum of the fuel requirements: {}", fuel.iter().sum::<u32>());
+
+    for f in fuel.iter_mut() {
+        *f += calculate_fuel_recursive(f);
+    }
+
+    println!("[PART TWO] sum of the fuel requirements: {}", fuel.iter().sum::<u32>());
+}
+
+fn calculate_fuel(mass: &u32) -> i32 {
+    (*mass as f32 / 3 as f32).floor() as i32 - 2
+}
+
+fn calculate_fuel_recursive(mass: &u32) -> u32 {
+    let fuel = calculate_fuel(mass);
+
+    if fuel.is_negative() {
+        return 0;
+    }
+
+    fuel as u32 + calculate_fuel_recursive(&(fuel as u32))
+}
