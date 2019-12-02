c1208e52a1641eb8a8926867196945499fec968b
Jan Baudisch <dev@baudisch.xyz>
2019-12-02 21:46:34 +0000

    day_02: make ranges inclusive

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -8,8 +8,8 @@ fn main() {
 
     println!("[PART ONE] value at position 0: {}", program.run(12, 2));
 
-    'outer: for noun in 0..100 {
-        for verb in 0..100 {
+    'outer: for noun in 0..=100 {
+        for verb in 0..=100 {
             program.reset();
             if program.run(noun, verb) == 19_690_720 {
                 println!("[PART TWO] 100 * noun + verb = {}", 100 * noun + verb);
